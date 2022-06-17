a1ce3e693769a3e112da931f12c37ef5b7dd682c
gokultw <gokul.kuppan@thoughtworks.com>
2022-06-17 14:17:31 +0530

    OpenElis Fixed

--- tests/lab/openELIS.js
+++ tests/lab/openELIS.js
@@ -1,37 +1,37 @@
 "use strict"
-const { goto, toRightOf, textBox,text, into, write, click, $,below, checkBox,waitFor,image,within,link, highlight, screenshot } = require('taiko');
+const { goto, toRightOf, textBox, text, into, write, click, $, below, checkBox, waitFor, image, within, link, highlight, screenshot } = require('taiko');
 var fileExtension = require('../util/fileExtension')
 const taikoHelper = require("../util/taikoHelper")
-step("Enter password in ELIS", async function() {
-    await write("adminADMIN!",into(textBox(toRightOf("Enter Password:"))));
+step("Enter password in ELIS", async function () {
+        await write("adminADMIN!", into(textBox(toRightOf("Enter Password:"))));
 });
 
-step("goto ELIS home", async function() {
+step("goto ELIS home", async function () {
         await goto(process.env.bahmniHost + process.env.openelisHome);
 });
 
-step("enter user name in ELIS", async function() {
-        await write("admin",into(textBox(toRightOf("Enter Username:"))));
+step("enter user name in ELIS", async function () {
+        await write("admin", into(textBox(toRightOf("Enter Username:"))));
 });
 
-step("click Submit", async function() {
-        await click("Submit");	
+step("click Submit", async function () {
+        await click("Submit");
 });
 
 step("Find the patient", async function () {
         var patientIdentifierValue = gauge.dataStore.scenarioStore.get("patientIdentifier");
-        await write(patientIdentifierValue,into(textBox(below("Patient Id"))));
+        await write(patientIdentifierValue, into(textBox(below("Patient Id"))));
 });
 
 step("Click collect sample", async function () {
-//        await waitFor(async () => (await link("Collect Sample").isVisible()))
+        //        await waitFor(async () => (await link("Collect Sample").isVisible()))
         await waitFor(1000)
         // await highlight(link("Collect Sample"))
         // await screenshot()
         await click(link("Collect Sample"));
 });
 
-step("Generate the auto id", async function() {
+step("Generate the auto id", async function () {
         await waitFor(2000)
         await click("Generate");
 });
@@ -44,57 +44,57 @@ step("Enter lab result <details> in the result", async function (details) {
         var content = fileExtension.parseContent(`./data/elis/samplesCollected/${details}.json`)
         var data = null;
         data = JSON.parse(content)
-        for(var resultIndx=0;resultIndx<data.results.length;resultIndx++){
+        for (var resultIndx = 0; resultIndx < data.results.length; resultIndx++) {
                 await write(data.results[resultIndx].value,
-                        into(textBox(toRightOf(data.results[resultIndx].name)))) 
-        }        
+                        into(textBox(toRightOf(data.results[resultIndx].name))))
+        }
 });
 
 step("Click today's sample in samples collected", async function () {
-        await click("Today",below("Samples Collected"));
+        await click("Today", below("Samples Collected"));
 });
 
-step("Click collect sample for <patientIdentifier>", async function(patientIdentifier) {
-        await click("Backlog",below("Samples to Collect"));
-        await write(patientIdentifier,into(textBox(below("Patient ID"))));
+step("Click collect sample for <patientIdentifier>", async function (patientIdentifier) {
+        await click("Backlog", below("Samples to Collect"));
+        await write(patientIdentifier, into(textBox(below("Patient ID"))));
         await click("Collect Sample");
 });
 
 step("Validate lab result <details>", async function (details) {
         var patientIdentifier = gauge.dataStore.scenarioStore.get("patientIdentifier")
-        await click(image({title:'Validate'}),toRightOf(patientIdentifier),toRightOf(text("No")))
+        await click($("//*[text()='" + patientIdentifier + "']/..//img[@title='Validate']"))
         var content = fileExtension.parseContent(`./data/elis/samplesCollected/${details}.json`)
         var data = null;
         data = JSON.parse(content)
-        for(var resultIndx=0;resultIndx<data.results.length;resultIndx++){
-                await click(checkBox(within($(`#row_${resultIndx}`),below("Accept"))));
+        for (var resultIndx = 0; resultIndx < data.results.length; resultIndx++) {
+                await click(checkBox(within($(`#row_${resultIndx}`), below("Accept"))));
         }
 });
 
-step("Put identifier <patientIdentifier>", async function(patientIdentifier) {
-        gauge.dataStore.scenarioStore.put("patientIdentifier",patientIdentifier);
+step("Put identifier <patientIdentifier>", async function (patientIdentifier) {
+        gauge.dataStore.scenarioStore.put("patientIdentifier", patientIdentifier);
 });
 
-step("Click backlog of sample collection", async function() {
-        await click("Backlog",below("Samples to Collect"));
+step("Click backlog of sample collection", async function () {
+        await click("Backlog", below("Samples to Collect"));
 });
 
-step("Open the result of the patient", async function() {
+step("Open the result of the patient", async function () {
         var patientIdentifier = gauge.dataStore.scenarioStore.get("patientIdentifier")
-        await click(image({title:'Result'},toRightOf(patientIdentifier)))
+        await click(image({ title: 'Result' }, toRightOf(patientIdentifier)))
         await taikoHelper.repeatUntilFound(text("Results"))
 });
 
-step("Click lab dashboard", async function() {
+step("Click lab dashboard", async function () {
         await click(link("Lab Dashboard"))
 });
 
-step("click Send Anyway", async function() {
-        try{
+step("click Send Anyway", async function () {
+        try {
                 await click("Send Anyway")
-        }catch(e){}
+        } catch (e) { }
 });
 
-step("Goto OpenELIS", async function() {
+step("Goto OpenELIS", async function () {
         await goto(process.env.bahmniHost + process.env.openelisHome, { waitForNavigation: true, navigationTimeout: process.env.actionTimeout });
 });
